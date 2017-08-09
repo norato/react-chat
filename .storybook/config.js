843d1c987214dce61d241bfaa0faa4602b441e38
Felipe Norato Lacerda <fnoratol@gmail.com>
2017-08-09 13:05:51 -0300

    Add spec addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
 import { configure } from '@storybook/react';
+import infoAddon from '@kadira/react-storybook-addon-info';
 import '../public/styles/main.css';
 
+
 function loadStories() {
-  // require('../src/stories/index');
   require('../src/stories/MessageField');
   require('../src/stories/ButtonSubmit');
   require('../src/stories/MessageForm');
   require('../src/stories/Message');
   require('../src/stories/MessageList'); 
 }
+// setAddon(infoAddon);
 
 configure(loadStories, module);
