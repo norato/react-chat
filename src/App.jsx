f1b66ecabc53e7ffa3347dde64788b8eddf10f7e
Felipe Norato Lacerda <fnoratol@gmail.com>
2017-08-09 11:53:28 -0300

    Change strategy on socket communication

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import io from "socket.io-client"
 
 import {
-  connect_to_server
-  , send_message
+  send_message
   , receive_message
 } from './actions'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.props.dispatch(connect_to_server())
-    this.state = {
-      messages : []
-    }
+    const SERVER_URL = `http://localhost:3001/`
+    this.socket = io(SERVER_URL)
   }
 
-  shouldComponentUpdate( nextProps, nextState ) {
-    if (this.props.socket) {
-      return this.props.socket.on('new message', (msg)=>{
-        this.props.dispatch(receive_message(msg))
-        return false;
-      })
-    }
-    return false
+  componentDidMount() {
+    this.socket.on('new message', (msg)=>{
+      this.props.dispatch(receive_message(msg))
+    })
   }
   __sendMessage(){
     let message = `Send Message ${Date()}`
-    this.props.dispatch(send_message({messages: message}))
+    this.props.dispatch(send_message({socket: this.socket, message: message}))
   }
 
   render() {
@@ -54,7 +48,6 @@ class App extends Component {
 const mapStateToProps = (state) => {
   return {
     Messages: state.get('messages')
-    , socket: state.get('socket')
   }
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,16 @@
 import Immutable from 'immutable';
 import {
-  CONNECT_TO_SERVER
-  , SEND_MESSAGE
+  SEND_MESSAGE
   , RECEIVE_MESSAGE
 } from '../actions'
 
 const initialState = {
   messages: []
-  , socket : ""
 }
 
 let defaultState = Immutable.fromJS(initialState);
 const reducer = (state=defaultState, action) => {
   switch (action.type) {
-    case CONNECT_TO_SERVER:
-      return state.mergeDeep({
-        socket: action.socket
-      })
     case SEND_MESSAGE:
       return state.updateIn(['messages'], arr => arr.push(action.message));
     case RECEIVE_MESSAGE:
